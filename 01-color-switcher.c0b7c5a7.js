40041a6f7df75b4be5490c62d264400497ca2a50
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-12-25 15:05:26 +0000

    Deploying to gh-pages from @ IgorShamborskiy/goit-js-hw-09@07f97d9bcf64ee169112508a05b0479ab35fbf03 🚀

---
+++ 01-color-switcher.c0b7c5a7.js
@@ -0,0 +1,2 @@
+[document.querySelector("button[data-start]"),document.querySelector("button[data-stop]")][0].addEventListener("clic",(function(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}));
+//# sourceMappingURL=01-color-switcher.c0b7c5a7.js.map
