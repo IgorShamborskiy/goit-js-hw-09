1680def789adb4fa8f2d7c9ada4f1ee10f9b5888
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-12-25 19:02:22 +0200

    add homework2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,12 +1,39 @@
 const refs = {
     buttonStart: document.querySelector('button[data-start]'),
-buttonStop: document.querySelector('button[data-stop]'),
+  buttonStop: document.querySelector('button[data-stop]'),
+}
+btnSwitcher(false, true); 
+
+refs.buttonStart.addEventListener("click", changeBgColor); 
+refs.buttonStop.addEventListener("click", stopBgColor); 
+let timerId ;
+
+function changeBgColor() { 
+  if (!timerId) { 
+    timerId = setInterval(onTime, 1000); 
+    btnSwitcher(true, false);
+  }
 }
 
-
-refs.buttonStart.addEventListener('clic', getRandomHexColor);
-// refs.buttonStop.addEventListener('clic', getRandomHexColor);
+function btnSwitcher(start,stop) {
+  refs.buttonStop.disabled = stop;
+  refs.buttonStart.disabled = start;
+}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function onTime() { 
+    document.body.style.background = getRandomHexColor(); 
+}
+
+function stopBgColor() { 
+  clearInterval(timerId);
+  timerId = null; 
+  btnSwitcher(false, true); 
+}
+
+
+
+
