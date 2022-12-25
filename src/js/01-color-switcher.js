07f97d9bcf64ee169112508a05b0479ab35fbf03
IgorShamborskiy <108817453+IgorShamborskiy@users.noreply.github.com>
2022-12-25 17:03:41 +0200

    add 3 homework

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,12 @@
+const refs = {
+    buttonStart: document.querySelector('button[data-start]'),
+buttonStop: document.querySelector('button[data-stop]'),
+}
 
+
+refs.buttonStart.addEventListener('clic', getRandomHexColor);
+// refs.buttonStop.addEventListener('clic', getRandomHexColor);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from "notiflix";
+const form = document.querySelector(`.form`);
+form.addEventListener(`submit`, onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault()
+  const { delay, step, amount } = event.target.elements;
+  let deleyValue = +delay.value
+  for (let index = 0; index <= amount.value; index+=1) {
+    createPromise(index, deleyValue)
+    .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+    deleyValue+=+step.value
   }
+  event.target.reset()
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay});
+  } else {
+        reject({position, delay});
+  }
+    },delay)
+  
+  })
+ 
 }
